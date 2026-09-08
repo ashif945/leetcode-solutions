4715da52338f267abd7cb6347bcc9677855fecaf
Md Ashif Ansari <ansariashif945@gmail.com>
2026-09-08 20:18:50 +0530

    Time: 0 ms (100%), Space: 42.8 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,13 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        int count=0;
-        for(int i=1000; i<=n; i++){
-            count++;
-        }
-        if(n < 1000){
-            return 0;
-        }  
-        return count;
+        if(n<1000) 
+        return 0;
+        return (n-999);
     }
     
 }
