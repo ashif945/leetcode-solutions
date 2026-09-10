7f393b4a7b3797e614881a6a6b5f3d47fc7bd795
Md Ashif Ansari <ansariashif945@gmail.com>
2026-09-11 02:09:08 +0530

    Time: 1 ms (96.62%), Space: 47.6 MB (8.53%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int leftSum = 0;
+        int totalSum = 0;
+        while(i<n){
+            totalSum+= nums[i];
+            i++;
+        }
+        i=0;
+        while(i<n){
+            int rightSum = totalSum - leftSum - nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum+=nums[i];
+            i++;
+        }
+        return -1;
+        
+    }
+}
